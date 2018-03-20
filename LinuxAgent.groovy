d6d88a955d67f90e11415d7ddf089b49bd40266f
Daksha Sapkale <Daksha.Sapkale@infostretch.com>
2018-03-20 16:30:06 +0530

    added new file

---
+++ LinuxAgent.groovy
@@ -0,0 +1,16 @@
+import jenkins.model.*
+import hudson.model.*
+import hudson.slaves.*
+import hudson.plugins.sshslaves.*
+import java.util.ArrayList;
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry;
+
+  Slave slave = new DumbSlave(
+                    "agent-node","Agent node description",
+                    "/home/daksha",
+                    "1",
+                    Node.Mode.NORMAL,
+                    "agent-node-label",
+                    new SSHLauncher("10.11.28.124",22,"root","root123","","","","",""),
+                    new RetentionStrategy.Always())
+         Jenkins.instance.addNode(slave)
