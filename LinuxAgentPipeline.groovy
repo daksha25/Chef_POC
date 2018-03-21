8cf1a90f12b684387d942e84f2acd887560ddf2c
Daksha Sapkale <Daksha.Sapkale@infostretch.com>
2018-03-21 17:28:41 +0530

    file added

--- LinuxAgentPipeline.groovy
+++ LinuxAgentPipeline.groovy
@@ -5,6 +5,6 @@ import hudson.plugins.sshslaves.*
 import java.util.ArrayList;
 
 Slave slave = new DumbSlave(Name,description,Slave_path,noofexecutors,Node.Mode.NORMAL,
-                    Labels,new SSHLauncher(Host,22,User,pass," "," "," "," "," "),
+                    Labels,new SSHLauncher(Host,22,root,pass," "," "," "," "," "),
                     new RetentionStrategy.Always())
  Jenkins.instance.addNode(slave)
