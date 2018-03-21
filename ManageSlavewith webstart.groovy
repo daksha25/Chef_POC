5b6d4f9261dedd566bb3e5a695dd686a142a5ea5
Daksha Sapkale <Daksha.Sapkale@infostretch.com>
2018-03-21 15:55:04 +0530

    changed

--- ManageSlavewith webstart.groovy
+++ ManageSlavewith webstart.groovy
@@ -1,15 +1,21 @@
 import jenkins.model.*
 import hudson.model.*
 import hudson.slaves.*
+import hudson.plugins.sshslaves.*
+import java.util.ArrayList;
 
-Jenkins.instance.addNode(
-  new DumbSlave(
-    "test-script",
-    "test slave description",
-    "/export/home/pe-deploy/",
-    "1",
-    Node.Mode.NORMAL,
-    "test-slave-label",
-    new JNLPLauncher(),
-    new RetentionStrategy.Always(),
-    new LinkedList()))
+
+def Name = build.buildVariableResolver.resolve('Name');
+def description = build.buildVariableResolver.resolve('description');
+def Slave_path =  build.buildVariableResolver.resolve('Slave_path');
+def noofexecutors = build.buildVariableResolver.resolve('noofexecutors');
+def Labels = build.buildVariableResolver.resolve('Labels');
+def Host = build.buildVariableResolver.resolve('Host');
+def User = build.buildVariableResolver.resolve('User');
+def pass = build.buildVariableResolver.resolve('pass');
+
+
+Slave slave = new DumbSlave(Name,description,Slave_path,noofexecutors,Node.Mode.NORMAL,
+                    Labels,new SSHLauncher(Host,22,User,pass," "," "," "," "," "),
+                    new RetentionStrategy.Always())
+ Jenkins.instance.addNode(slave)
